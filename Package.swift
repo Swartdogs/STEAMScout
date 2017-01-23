6765f34bbf1fdfa5166e95d3ce34c92d6e525128
Srinivas Dhanwada <dhanwads@gmail.com>
2017-01-23 01:08:08 -0600

    Added Package.swift - Renamed Test folders to remove conflict with SPM

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "SteamScout",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
+    ]
+)
+
