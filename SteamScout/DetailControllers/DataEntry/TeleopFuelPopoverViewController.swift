f8782e9f716f5720af9b597c038142c1422b8d77
Srinivas Dhanwada <dhanwads@gmail.com>
2017-03-14 02:23:39 -0500

    Add control for Teleop Fuel Popovers

--- SteamScout/DetailControllers/DataEntry/TeleopFuelPopoverViewController.swift
+++ SteamScout/DetailControllers/DataEntry/TeleopFuelPopoverViewController.swift
@@ -1,48 +1,51 @@
 //
-//  TeleopViewController.swift
+//  TeleopFuelPopoverViewController.swift
 //  SteamScout
 //
-//  Created by Dylan Wells on 2/13/17.
+//  Created by Srinivas Dhanwada on 3/14/17.
 //  Copyright © 2017 dhanwada. All rights reserved.
 //
 
 import UIKit
 
-class TeleopViewController: UIViewController {
+class TeleopFuelPopoverViewController: UIViewController {
     
     var match = SteamMatch()
     
-    @IBOutlet weak var valueLabel: UILabel!
-    @IBOutlet weak var stepper: UIStepper!
-    
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
-            match.teleGearsScored = Int(sender.value)
-            valueLabel.text = Int(sender.value).description
-    }
+    weak var teleopView: TeleopViewController!
 
+    @IBOutlet weak var HighGoalSlider: UISlider!
+    @IBOutlet weak var LowGoalSlider: UISlider!
+    
+    @IBAction func GoalSliderChanged(_ sender: UISlider) {
+        if(sender.tag == 0) {
+            match.teleHighFuelScored = sender.value
+        } else if(sender.tag == 1) {
+            match.teleLowFuelScored = sender.value
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        stepper.wraps = false
-        stepper.autorepeat = false
-        stepper.maximumValue = 12
-        stepper.stepValue = 1
-        
+
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        match = MatchStore.sharedStore.currentMatch as! SteamMatch
+        match = teleopView.match
         
-        stepper.value = Double(match.teleGearsScored)
-        valueLabel.text = "\(match.teleGearsScored)"
+        HighGoalSlider.value = match.teleHighFuelScored
+        LowGoalSlider.value = match.teleLowFuelScored
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        MatchStore.sharedStore.updateCurrentMatchForType(.teleop, match: match)
+        teleopView.match.teleLowFuelScored = match.teleLowFuelScored
+        teleopView.match.teleHighFuelScored = match.teleHighFuelScored
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,16 +54,14 @@ class TeleopViewController: UIViewController {
     }
     
 
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
-        if(segue.identifier == "showFuelPopover") {
-            let popover = segue.destination as! TeleopFuelPopoverViewController
-            popover.teleopView = self
-        }
     }
+    */
+
 }
